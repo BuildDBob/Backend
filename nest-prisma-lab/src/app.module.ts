fde64d0191d39b1807a7896e51b5d70156197e06
BuildDBob <90126152+BuildDBob@users.noreply.github.com>
2026-09-25 10:15:26 +0700

    Update app.module.ts

--- nest-prisma-lab/src/app.module.ts
+++ nest-prisma-lab/src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { AuthModule } from './auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
-  imports: [PrismaModule, RoomsModule, AuthModule],
+  imports: [PrismaModule, RoomsModule, AuthModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: await redisStore({
+          socket: {
+            host: '127.0.0.1',
+            port: 6379
+          },
+        }),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
